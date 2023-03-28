d795e4a3a1a4f9b68b3c40bb22f83d8f2b4bd629
Sergio Medina <hola@sergiomedina.io>
2023-03-27 22:41:07 -0300

    feat: add fetch request

--- clase_03/ajax_request/main.js
+++ clase_03/ajax_request/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
     const element = document.getElementById('button');
+    const buttonWithFetch = document.getElementById('buttonWithFetch');
     const container = document.getElementById('container');
 
     element.addEventListener('click', function() {
@@ -17,5 +18,18 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         });
         request.send();
+    });
+
+    buttonWithFetch.addEventListener('click', function() {
+        fetch('https://rickandmortyapi.com/api/character') // fetch devuelve una promesa
+            .then(response => response.json()) // response.json() devuelve una promesa
+            .then(data => {
+                let html = '<ul>';
+                data.results.forEach(function(item) {
+                    html += `<li>${item.name}</li>`;
+                });
+                html += '</ul>';
+                container.innerHTML = `<h1>Con Fetch</h1>${html}`;
+            });
     });
 });
