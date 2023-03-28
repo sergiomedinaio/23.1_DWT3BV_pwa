6d4c645708eba71f0eb9cc74a0e5d7c58a8691f3
Sergio Medina <hola@sergiomedina.io>
2023-03-27 22:11:32 -0300

    feat: add json request

--- clase_03/ajax_json/main.js
+++ clase_03/ajax_json/main.js
@@ -2,7 +2,18 @@ window.addEventListener('DOMContentLoaded', function() {
     const btn = document.querySelector('#btn');
     const div = document.querySelector('#div');
     btn.addEventListener('click', function() {
-       console.log("prueba de click");
-       console.log(div)
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost/23.1_DWT3BV_pwa/clase_03/ajax_json/persona.json');
+        xhr.addEventListener('readystatechange', function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const {name, lastName, age} = JSON.parse(xhr.responseText);
+                div.innerHTML = `
+                    <p>Nombre: ${name}</p>
+                    <p>Apellido: ${lastName}</p>
+                    <p>Edad: ${age}</p>
+                `;
+            }
+        });
+        xhr.send();
     });
 });
