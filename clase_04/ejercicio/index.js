1cd4bef34838de5339c1c88e521c028fcae6de34
Sergio Medina <hola@sergiomedina.io>
2023-04-03 22:06:46 -0300

    feat: add api request

--- clase_04/ejercicio/index.js
+++ clase_04/ejercicio/index.js
@@ -4,10 +4,20 @@ const app = new Vue(
         data: {
             titulo: 'Hola mundo',
             mensaje: 'Hola mundo desde Vue',
+            listaDeNombres: []
         },
         methods: {
             saludar() {
-                alert('Hola mundo')
+                const URL = 'https://rickandmortyapi.com/api/character';
+                const convertirEnJSON = (response) => response.json();
+                const extraerNombres = (data) => data.results.map(personaje => personaje.name);
+                const actualizarListaDeNombres = nombres => this.listaDeNombres = nombres;
+
+                fetch(URL) // fetch devuelve una promesa
+                    .then(convertirEnJSON) // convierte la respuesta en un objeto json
+                    .then(extraerNombres) // extrae el array de nombres
+                    .then(actualizarListaDeNombres) // asigna el array de nombres a la propiedad listaDeNombres
+
             }
         }
     }
