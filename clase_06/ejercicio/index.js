98406a424e7b51d8c3c9333ccbf6310cfc75a1e1
Sergio Medina <hola@sergiomedina.io>
2023-04-17 21:28:23 -0300

    chore: add initial sw setup

---
+++ clase_06/ejercicio/index.js
@@ -0,0 +1,86 @@
+const URL = 'https://rickandmortyapi.com/api/character';
+const convertirEnJSON = (response) => response.json();
+const extraerNombres = (data) => data.results.map(personaje => personaje.name);
+const extraerGenero = (data) => data.results.map(personaje => personaje.gender);
+const esperarDosSegundos = () => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Se resolvio todo ok");
+    }, 5000);
+});
+
+//const serviceWorkerEnable = navigator.serviceWorker && navigator.serviceWorker.register;
+
+if(navigator.serviceWorker && navigator.serviceWorker.register) {
+    navigator.serviceWorker.register('/sw.js')
+    .then((reg) => {
+            console.log('Service worker registrado');
+            if(reg.installing) {
+                console.log('Service worker instalando');
+            } else if(reg.waiting) {
+                console.log('Service worker instalado');
+            } else if(reg.active) {
+                console.log('Service worker activo');
+            }
+    })
+    .catch((err) => {
+        console.log('Error al registrar el service worker', err);
+    });
+}
+
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            titulo: 'Hola mundo',
+            mensaje: 'Hola mundo desde Vue',
+            listaDeNombres: []
+        },
+        methods: {
+
+            async presentarListaDeNombres() {
+                const actualizarListaDeNombres = nombres => this.listaDeNombres = nombres;
+
+                const nombres = await fetch(URL).then(extraerNombres);
+                await esperarDosSegundos();
+                const generos = await fetch(URL).then(convertirEnJSON).then(extraerGenero);
+                const nombresConGenero = nombres.map((nombre, index) => {
+                    return generos[index] + ' - ' + nombre;
+                });
+                actualizarListaDeNombres(nombresConGenero);
+
+            },
+            async saludar() {
+                const actualizarListaDeNombres = nombres => this.listaDeNombres = nombres;
+
+                const query = Promise.all(
+                    [
+                            fetch(URL).then(convertirEnJSON).then(extraerNombres),
+                            fetch(URL).then(convertirEnJSON).then(extraerGenero),
+                            esperarDosSegundos()
+                        ]
+                )
+
+                const [nombres, generos] = await query;
+                const nombresConGenero = nombres.map((nombre, index) => {
+                    return generos[index] + ' - ' + nombre;
+                });
+                actualizarListaDeNombres(nombresConGenero);
+
+                /*
+                    //Lo que estaba antes
+                    query.then((respuestas) => {
+                    const nombres = respuestas[0];
+                    const generos = respuestas[1];
+                    const nombresConGenero = nombres.map((nombre, index) => {
+                        return generos[index] + ' - ' + nombre;
+                    });
+                    actualizarListaDeNombres(nombresConGenero);
+
+                });*/
+
+
+
+            }
+        }
+    }
+)
